ed8343e64c1ec9a9009014910b852f2f148b8bcc
CoffeManFP <coffemanfp@gmail.com>
2020-07-20 19:53:11 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sprinkle
+
+This program add som web-friendly sprinkle words to increase the chances of finding available domain names. Add some sugar terms in order to improve the odds of finding available names.
+
+For examples, if we pass in the word ``chat``, it might pass out ``chatapp``; alternatively, if we pass in ``talk``, we may get back ``talk time``.
+
+
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+// otherWord is a helper value for to get the value to replace
+const otherWord = "*"
+
+// transforms are simple words posibilities
+var transforms = []string{
+	otherWord,
+	otherWord,
+	otherWord,
+	otherWord,
+
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get " + otherWord,
+	"go" + otherWord,
+	"lets" + otherWord,
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	// Reading input
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+
+		// Select a random transform
+		t := transforms[rand.Intn(len(transforms))]
+
+		// Replace the input and print the result
+		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	}
+}
