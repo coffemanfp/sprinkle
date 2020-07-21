cb5681b7358e33c2cdb6e51765471d1cf3069f32
CoffeManFP <coffemanfp@gmail.com>
2020-07-21 15:48:53 -0400

    Going back to previous state

--- main.go
+++ main.go
@@ -2,27 +2,60 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
 )
 
+// wordsFilepath is the flag for a different words filepath
+var wordsFilepath string
+
 // otherWord is a helper value for to get the value to replace
 const otherWord = "*"
 
-// transforms are simple words posibilities
-var transforms = []string{
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get " + otherWord,
-	"go" + otherWord,
-	"lets" + otherWord,
+// Transforms model for the words file
+type Transforms struct {
+	Words Words `json:"words"`
+}
+
+type Words struct {
+	Before []string `json:"before"`
+	After  []string `json:"after"`
 }
 
 func main() {
+	wordsFileBytes, err := ioutil.ReadFile(wordsFilepath)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var transforms Transforms
+
+	err = json.Unmarshal(wordsFileBytes, &transforms)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var allTransforms []string
+
+	for _, word := range transforms.Words.Before {
+		word = word + otherWord
+		allTransforms = append(allTransforms, word)
+	}
+
+	for _, word := range transforms.Words.After {
+		word = otherWord + word
+		allTransforms = append(allTransforms, word)
+	}
+
+	log.Println(allTransforms)
+
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	// Reading input
@@ -30,9 +63,19 @@ func main() {
 	for s.Scan() {
 
 		// Select a random transform
-		t := transforms[rand.Intn(len(transforms))]
+		t := allTransforms[rand.Intn(len(allTransforms))]
 
 		// Replace the input and print the result
 		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
 	}
+}
+
+func init() {
+	initFlags()
+}
+
+func initFlags() {
+	flag.StringVar(&wordsFilepath, "wordsFile", "words.json", "A file with words to transform")
+
+	flag.Parse()
 }
